51aab68514ce9d39aa0191748d924a648a9fd69f
Liuming Chen <51524534+let-lc@users.noreply.github.com>
2022-01-12 23:56:34 -0500

    fix: update result item styles
    * fix thumbnail shrink issue
    
    * adjust & add styles for mobile version
    
    * adjust skeleton to match the new styles
    
    * fixed channel name truncate issue

--- src/lib/components/ResultItem.skeleton.svelte
+++ src/lib/components/ResultItem.skeleton.svelte
@@ -1,17 +1,21 @@
 <div
-  class="flex w-full p-1 text-white transition-colors duration-150 rounded-sm bg-white/5 gap-x-4"
+  class="relative flex flex-col w-full p-1 space-x-0 rounded-sm sm:space-x-4 sm:flex-row bg-white/5"
 >
-  <div
-    class="object-cover w-48 min-w-[12rem] min-h-[7rem] rounded select-none h-28 bg-white/25 animate-pulse"
-  />
-  <div class="flex flex-col justify-between w-full">
-    <div class="space-y-1">
-      <div
-        class="w-11/12 h-4 font-medium truncate whitespace-normal rounded-sm group-hover:underline bg-white/25 animate-pulse"
-      />
-      <div class="h-4 text-sm rounded-sm hover:underline bg-white/25 w-36 animate-pulse" />
-      <div class="text-xs pt-0.5 h-3 bg-white/25 rounded-sm w-24 animate-pulse" />
+  <div class="w-full h-auto rounded sm:w-48 aspect-video bg-white/25 animate-pulse" />
+  <div class="flex flex-col justify-between w-full pt-2 space-y-1 sm:pt-0">
+    <div
+      class="w-full h-4 font-medium truncate whitespace-normal rounded-sm sm:w-11/12 group-hover:underline bg-white/25 animate-pulse"
+    />
+    <div class="flex flex-row items-center flex-grow space-y-1 sm:flex-col sm:items-start">
+      <div class="flex-grow">
+        <div
+          class="h-3.5 sm:h-4 text-sm rounded-sm hover:underline bg-white/25 w-36 animate-pulse"
+        />
+      </div>
     </div>
-    <div class="h-3.5 text-xs rounded-sm bg-white/25 w-16 animate-pulse" />
+    <div class="flex flex-row sm:flex-col justify-between">
+      <div class="text-xs sm:pt-0.5 h-3 bg-white/25 rounded-sm w-24 animate-pulse" />
+      <div class="w-16 h-3 text-xs rounded-sm bg-white/25 animate-pulse sm:mt-1 mt-0" />
+    </div>
   </div>
 </div>
--- src/lib/components/ResultItem.svelte
+++ src/lib/components/ResultItem.svelte
@@ -20,10 +20,10 @@
 </script>
 
 <div
-  class="flex p-1 text-white transition-colors duration-150 bg-white bg-opacity-0 rounded-sm gap-x-4 hover:bg-opacity-5"
+  class="relative flex flex-col items-stretch p-1 space-x-0 text-white transition-colors duration-150 bg-white bg-opacity-0 rounded-sm sm:flex-row sm:space-x-4 hover:bg-opacity-5"
 >
   <button
-    class="relative overflow-hidden border border-transparent rounded group hover:border-white/25"
+    class="relative flex-shrink-0 overflow-hidden border border-transparent rounded group hover:border-white/5"
     on:click={loadAudio}
   >
     <div
@@ -61,25 +61,42 @@
         </svg>
       {/if}
     </div>
-
-    <img src={result.thumbnail} alt="Thumbnail" class="object-cover w-48 select-none h-28" />
+    <!-- Thumbnail -->
+    <img
+      src={result.thumbnail}
+      alt="Thumbnail"
+      loading="lazy"
+      class="object-cover w-full select-none aspect-video sm:w-48"
+    />
   </button>
-  <div class="flex flex-col justify-between">
-    <div>
-      <a href={result.url} rel="noreferrer" target="_blank" class="group">
-        <h1 class="font-medium truncate whitespace-normal group-hover:underline">
+  <div class="flex flex-col h-full px-1 pt-1 overflow-hidden sm:flex-grow sm:pt-0 sm:px-0">
+    <div class="flex-grow">
+      <a href={result.url} rel="noreferrer" target="_blank">
+        <h1
+          class="text-sm font-medium whitespace-normal line-clamp-2 hover:underline sm:text-base"
+          title={result.title}
+        >
           {result.title}
         </h1>
       </a>
       <a href={result.channelUrl} rel="noreferrer" target="_blank">
-        <span class="text-sm hover:underline">{result.channel}</span>
+        <p
+          class="text-xs truncate whitespace-normal sm:text-sm hover:underline"
+          title={result.channel}
+        >
+          {result.channel}
+        </p>
       </a>
-      <p class="text-xs pt-0.5">{result.views?.toLocaleString()} views</p>
     </div>
-    {#if result.duration}
-      <p class="px-1 py-px mb-1 text-xs rounded-sm bg-black/25 w-max">
-        {result.duration}
-      </p>
-    {/if}
+    <div class="flex flex-row items-center sm:items-start sm:flex-col">
+      <p class="text-xs">{result.views?.toLocaleString()} views</p>
+      {#if result.duration}
+        <div class="flex-grow sm:flex-grow-0">
+          <p class="float-right px-1 py-px text-xs rounded-sm bg-black/25 w-max sm:mt-1">
+            {result.duration}
+          </p>
+        </div>
+      {/if}
+    </div>
   </div>
 </div>
