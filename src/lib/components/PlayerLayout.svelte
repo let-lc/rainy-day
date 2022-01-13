52155cd4233aaf49a1fa8d0a656d990f60f25a78
Liuming Chen <51524534+let-lc@users.noreply.github.com>
2022-01-12 23:59:11 -0500

    fix: mobile version style & function update
    * fix browsers not support flexbox gap style
    
    * hide mobile keyboard when search triggered
    
    * adjust result items styles on mobile, displaying as two column grid

--- src/lib/components/PlayerLayout.svelte
+++ src/lib/components/PlayerLayout.svelte
@@ -29,7 +29,11 @@
   };
 
   const searchOnEnter: svelte.JSX.KeyboardEventHandler<HTMLInputElement> = (e) => {
-    if (e.key === 'Enter') search();
+    if (e.key === 'Enter') {
+      search();
+      // quit keyboard on mobile
+      e.currentTarget.blur();
+    }
   };
 </script>
 
@@ -37,16 +41,16 @@
   {#if $fullScreen}
     <PlayerFullscreen />
   {:else}
-    <div class="flex flex-col items-center justify-center flex-grow py-4 gap-y-4">
-      <div class="flex flex-col items-center w-full gap-y-4">
+    <div class="flex flex-col items-center justify-center flex-grow py-4 mx-4 space-y-4">
+      <div class="flex flex-col items-center w-full">
         <!-- Top Logo -->
         {#if !results.length && !loading}
-          <Logo class="h-16 text-white/80" />
+          <Logo class="h-16 mb-4 text-white/80" />
         {/if}
-        <div class="flex items-center justify-center w-full max-w-7xl gap-x-4">
+        <div class="flex items-center justify-center w-full space-x-4 max-w-7xl">
           <!-- Small Logo -->
           {#if results.length || loading}
-            <Logo class="h-6 text-white/80" />
+            <Logo class="h-7 text-white/80" />
           {/if}
           <!-- Search Input -->
           <div
@@ -76,7 +80,7 @@
         </div>
         <!-- Links -->
         {#if !results.length && !loading}
-          <div class="fixed bottom-2 text-xs text-center">
+          <div class="fixed text-xs text-center bottom-2">
             {#if typeof window !== 'undefined' && !window?.['chrome']}
               <p class="text-white/75">⚠️ Use chromium for the best visual effect.</p>
             {/if}
@@ -94,19 +98,27 @@
         {/if}
       </div>
       {#if loading}
-        <div class="flex-grow w-full h-0 max-w-5xl overflow-y-auto backdrop-blur bg-white/10">
-          <div class="grid px-1 gap-y-2">
-            {#each Array(10) as _, _i}
-              <ResultItemSkeleton />
-            {/each}
+        <div
+          class="relative flex-grow w-full max-w-5xl overflow-hidden rounded backdrop-blur bg-white/10"
+        >
+          <div class="absolute overflow-y-auto inset-2">
+            <div class="grid gap-2 px-1 grid-cols-2 sm:grid-cols-1">
+              {#each Array(10) as _, _i}
+                <ResultItemSkeleton />
+              {/each}
+            </div>
           </div>
         </div>
       {:else if results.length}
-        <div class="flex-grow w-full h-0 max-w-5xl overflow-y-auto backdrop-blur-sm bg-white/10">
-          <div class="grid px-1 gap-y-2">
-            {#each results as result}
-              <ResultItem {result} />
-            {/each}
+        <div
+          class="relative flex-grow w-full h-full max-w-5xl overflow-hidden rounded backdrop-blur bg-white/10"
+        >
+          <div class="absolute overflow-y-auto inset-2">
+            <div class="grid px-1 gap-2 grid-cols-2 sm:grid-cols-1">
+              {#each results as result}
+                <ResultItem {result} />
+              {/each}
+            </div>
           </div>
         </div>
       {/if}
